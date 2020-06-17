7c557e8361528a325906ac1e2089386da177f8de
vitmal143 <57004782+vitmal143@users.noreply.github.com>
2020-06-17 17:43:25 +0530

    string _reverse program file uploaded
    it is added to rveiew the code

---
+++ string_reverse.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    char str[1000], rev[1000];
+    int i, j, count = 0;
+    printf("Enter the string:");
+    scanf("%s", str);
+
+    while (str[count] != '\0')
+    {
+        count++;
+    }
+    j = count - 1;
+
+    for (i = 0; i < count; i++)
+    {
+        rev[i] = str[j];
+        j--;
+    }
+    rev[i]='\0';
+    printf("\nreversed string is %s",rev);
+    return 0;
+}
